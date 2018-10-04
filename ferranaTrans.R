0bd4b30a5824aaaa69235a63cfe741c2e49035b4
Gabriel Melo <gabriel.melo@iesp.uerj.br>
2018-10-04 14:03:43 -0300

    Nomes dos partidos corretos

--- ferranaTrans.R
+++ ferranaTrans.R
@@ -3,7 +3,7 @@ library(rio)
 library(tidyverse)
 
 ferranao <- import("~/Documentos/ferranaData/ferranao.xlsx")
-ferranao %>% 
+ferranao <- ferranao %>% 
   mutate(Partido = case_when(Partido == "MDB/PMDB" ~ "MDB",
           Partido == "PMDB" ~ "MDB",
           Partido == "PFL" ~ "PFL/DEM",
@@ -13,8 +13,9 @@ ferranao %>%
           Partido == "PSD" & ANO > 2011 ~ "PSD3",
           Partido == "PTB" & ANO < 1980 ~ "PTB1",
           Partido == "PTB" & ANO > 1981 ~ "PTB2",
-         TRUE ~ as.character(Partido))) %>% 
-  tabyl(Partido)
+          Partido == "PC do B" | Partido == "PC do B" | Partido == "PCdoB" | Partido == "PC DO B" ~ "PCDOB",
+          Partido == "PTdoB" | Partido == "PT do B" | Partido == "PT DO B" ~ "PTDOB",
+         TRUE ~ as.character(Partido)))
   
 
 ferranao$Partido <- as.factor(ferranao$Partido)
